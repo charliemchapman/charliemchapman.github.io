956fb27ffa64c092ae4889ece22a74d6f32e4a60
Charlie Chapman <charliemchapman@gmail.com>
2017-04-24 22:45:27 -0500

    fun designing

--- charlie-site/src/components/App.js
+++ charlie-site/src/components/App.js
@@ -5,10 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="app__hi">
-          <div className="h">h</div>
-          <div className="i">i</div>
-          <div className="p">.</div>
+        <div className="app__header">
+          <div className="app__hi">
+            <div className="h">h</div>
+            <div className="i">i</div>
+            <div className="p">.</div>
+          </div>
+        </div>
+        <div className="para">
+          <p>Hi there!  This obnoxiously large header with epic whitespace and a pithy word is here to show you how fun and elegant I can be.</p>
         </div>
       </div>
     );
