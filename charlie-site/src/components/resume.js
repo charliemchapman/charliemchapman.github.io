02729e30fc219ecc7ffe5b9c5b7c70df79c4f3b3
Charlie Chapman <charliemchapman@gmail.com>
2017-07-10 16:39:01 -0500

    fixed link to avoid cert error

--- charlie-site/src/components/resume.js
+++ charlie-site/src/components/resume.js
@@ -51,7 +51,7 @@ class Resume extends Component {
             <div className="para">Quick Links</div>
             <div className="external-buttons">
               <a href="https://drive.google.com/open?id=0B3RH7R64TJImaXJIdmE0LU1DdVk">Resume (pdf)</a>
-              <a href="https://video.charliemchapman.com">Video Portfolio</a>
+              <a href="http://video.charliemchapman.com">Video Portfolio</a>
             </div>
           </section>
         </Waypoint>
